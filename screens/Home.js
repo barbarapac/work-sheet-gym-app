ff11598e1f6dcab460ed404e92d5f26620e99225
Bárbara Antunes Pacheco <babiantunesp@gmail.com>
2022-09-16 21:39:31 -0300

    feat: inicio pagina home

--- screens/Home.js
+++ screens/Home.js
@@ -5,15 +5,56 @@ import Settings from '../screens/Settings';
 import Workout from '../screens/Workout';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="AvaliationDescription" component={AvaliationDescription} />
-      <Tab.Screen name="Settings" component={Settings} />
-      <Tab.Screen name="Workout" component={Workout} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarInactiveBackgroundColor: "#8A2BE2",
+        tabBarActiveTintColor: "#fff",
+        tabBarInactiveTintColor: "#D3D3D3",
+        tabBarActiveBackgroundColor: "#8A2BE2",
+        tabBarStyle:{ height: 70}
+      }}
+    >
+      <Tab.Screen
+        name="Workout"
+        component={Workout}
+        options={{
+          tabBarLabel: 'Página Inicial',
+          tabBarLabelStyle:{fontSize: 16, paddingBottom: 10},
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AvaliationDescription"
+        component={AvaliationDescription}
+        options={{
+          tabBarLabel: 'Avaliações',
+          tabBarLabelStyle:{fontSize: 16, paddingBottom: 10},
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="clipboard-text" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarLabel: 'Configurações',
+          tabBarLabelStyle:{fontSize: 16, paddingBottom: 10},
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -25,33 +66,8 @@ class Home extends Component{
             <NavigationContainer independent={true}>
                 <MyTabs />
             </NavigationContainer>
-            // <View style={styles.container}>
-            //     <View style={styles.content}>
-            //         <View style={{padding: 30}}>
-            //         </View>
-            //     </View>            
-            //     <View style={styles.footer}>
-            //         <Text>Footer</Text>
-            //     </View>
-            // </View>
         );
     }
 }
 
 export default Home;
-
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    content:{
-        flex: 1,
-        justifyContent: 'space-around', 
-    },
-    footer:{
-        backgroundColor: '#3c78d8',
-        justifyContent: 'center',
-        height: 70
-    }
-  });
